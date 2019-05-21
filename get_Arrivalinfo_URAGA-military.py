12303eb2fde6db46b8c219268f6390e4cce98c77
D Nishiyama <nishiyama.daiki.tu@alumni.tsukuba.ac.jp>
2019-05-21 14:23:43 +0900

    change. note only when info changed

--- get_Arrivalinfo_URAGA-military.py
+++ get_Arrivalinfo_URAGA-military.py
@@ -1,52 +1,59 @@
-import requests
+import requests, os, time, datetime
 from bs4 import BeautifulSoup
-import os
-import time
+
+all_vessels = []
 
 
 def search(elems):
+    latest_vessels = []
     hit = False
     for elem in elems:
         if elem.getText() == "官船":
             if not hit:
-                print("!HIT!\n")
+                print("HIT!", datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S'), "\n")
                 hit = True
-            print("data:\t\t", elem.find_previous().find_previous().getText())
-            print("name:\t\t", elem.find_previous().getText())
-            vesels.append(str(elem.find_previous().getText()))
-            print("type:\t\t", elem.getText())
+            print("data:\t\t\t", elem.find_previous().find_previous().getText())
+            name = elem.find_previous().getText()
+            print("name:\t\t\t", name)
+            latest_vessels.append(str(elem.find_previous().getText()))
+            print("type:\t\t\t", elem.getText())
             elem = elem.find_next().find_next().find_next().find_next()
-            print("country:\t", elem.getText())
+            print("country:\t\t", elem.getText())
             elem = elem.find_next()
-            print("destination\t", elem.getText(), "\n")
-
+            print("destination:\t", elem.getText(), "\n")
     if not hit:
-        print("Not Found.")
+        print("Not Found.", datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S'), "\n")
+
+    return latest_vessels
 
 
-def note():
-    if len(vesels) >= 1:
+def note(vessels):
+    if len(vessels) >= 1:
         ves_str = ""
-        for vessel in vesels:
+        for vessel in vessels:
             ves_str += vessel + ", "
         ves_str = ves_str[:-2]
         os.system(
             "osascript -e 'display notification \"{}\" with title \"新規入港予定情報取得\"'".format(ves_str))
 
 
-def main():
-    search(Elems)
-    note()
+def main(all):
+    target_url = 'https://www6.kaiho.mlit.go.jp/tokyowan/schedule/URAGA/schedule_3.html'
+    try:
+        r = requests.get(target_url)
+        soup = BeautifulSoup(r.content, 'lxml')
+        elems = soup.select('td')
+        latest = search(elems)
+        if latest != all:
+            all = latest
+            note(all)
+    except Exception as e:
+        print("ConnectionError, ", e)
+    finally:
+        return all
 
 
 while True:
-    target_url = 'https://www6.kaiho.mlit.go.jp/tokyowan/schedule/URAGA/schedule_3.html'
-    r = requests.get(target_url)  # requestsを使って、webから取得
-
-    soup = BeautifulSoup(r.content, 'lxml')
-
-    Elems = soup.select('td')
-    vesels = []
-
-    main()
-    time.sleep(10)
+    all_vessels = main(all_vessels)
+    print("-------------------------")
+    time.sleep(60)
